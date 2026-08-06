b53df4b9799f4b916c559d47a61cb8466eaf8df5
Esther Yaguana <yaguanalazo01@gmail.com>
2026-08-06 15:15:01 -0500

    feat: proyecto inicial GymTrack con estructura MVVM

--- app/src/main/java/com/example/gymtrack/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/gymtrack/ui/screens/HomeScreen.kt
@@ -1,4 +1,41 @@
 package com.example.gymtrack.ui.screens
 
-class HomeScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Text(
+            text = "👋 Bienvenido",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Text(
+            text = "GymTrack",
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        Text(
+            text = "Tu entrenador personal",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+    }
 }

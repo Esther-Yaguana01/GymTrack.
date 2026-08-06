4cf7c9e2adedfbb25913c2d5744d109aa50108af
Esther Yaguana <yaguanalazo01@gmail.com>
2026-08-06 15:15:01 -0500

    feat: integrar API de ejercicios con Retrofit

--- app/src/main/java/com/example/gymtrack/MainActivity.kt
+++ app/src/main/java/com/example/gymtrack/MainActivity.kt
@@ -5,33 +5,44 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.example.gymtrack.ui.screens.HomeScreen
-import com.example.gymtrack.ui.theme.GymTrackTheme
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gymtrack.ui.theme.GymTrackTheme
+
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-
         setContent {
             GymTrackTheme {
-
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { paddingValues ->
-
-                    HomeScreen(
-                        modifier = Modifier.padding(paddingValues)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
                     )
-
                 }
-
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GymTrackTheme {
+        Greeting("Android")
     }
 }
